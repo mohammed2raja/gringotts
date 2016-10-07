9873a494bb879c61da242152ecce9befb64718c7
Eugene Tiutiunnyk <eugene.tiutiunnyk@lookout.com>
2016-10-06 18:01:32 -0700

    Release v1.0.27

--- views/base/collection-view.js
+++ views/base/collection-view.js
@@ -1,6 +1,7 @@
 (function() {
   var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
+    hasProp = {}.hasOwnProperty,
+    indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   define(function(require) {
     var Chaplin, CollectionView, ServiceErrorReady, StringTemplatable, utils;
@@ -24,6 +25,28 @@
       CollectionView.prototype.animationStartClass = 'fade';
 
       CollectionView.prototype.animationEndClass = 'in';
+
+      CollectionView.prototype.modelsBy = function(rows) {
+        var itemViews, models;
+        itemViews = _.values(this.getItemViews());
+        return models = _.filter(itemViews, function(v) {
+          var ref;
+          return ref = v.el, indexOf.call(rows, ref) >= 0;
+        }).map(function(v) {
+          return v.model;
+        });
+      };
+
+      CollectionView.prototype.rowsBy = function(models) {
+        var itemViews, rows;
+        itemViews = _.values(this.getItemViews());
+        return rows = _.filter(itemViews, function(v) {
+          var ref;
+          return ref = v.model, indexOf.call(models, ref) >= 0;
+        }).map(function(v) {
+          return v.el;
+        });
+      };
 
       return CollectionView;
 
